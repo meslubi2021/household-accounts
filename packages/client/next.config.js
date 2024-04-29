9be16b45e9051b9f2222d34212d8ed54d6279f0b
Jehyun Jung <congmul@hotmail.com>
2024-04-28 21:23:04 -0700

    account-serivce-init

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -12,6 +12,10 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  i18n: {
+    defaultLocale: 'en-US',
+    locales: ['en-US', 'ko']
+  }
 };
 
 const plugins = [
