b9c027389ed6eb16e09aca0dffffdd4bd639f023
Jehyun Jung <congmul@hotmail.com>
2024-07-21 11:24:42 -0700

    deploy

--- packages/client/middleware.ts
+++ packages/client/middleware.ts
@@ -5,7 +5,7 @@ import { fallbackLng, languages, cookieName } from "./app/i18n/settings";
 acceptLanguage.languages(languages);
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)']
+    matcher: ['/((?!api|_next/static|_next/image|assets|.swa/health|favicon.ico|sw.js).*)']
 }
 
 
