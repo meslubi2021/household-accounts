6bf53f4c5ab05033c202e1e78f67a7889988ab45
Jehyun Jung <congmul@hotmail.com>
2024-07-21 22:38:12 -0700

    Get back original middleware

--- packages/client/middleware.ts
+++ packages/client/middleware.ts
@@ -10,26 +10,26 @@ export const config = {
 
 
 export function middleware(req: any) {
-    // let lng
-    // if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName).value)
-    // if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'))
-    // if (!lng) lng = fallbackLng
+    let lng
+    if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName).value)
+    if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'))
+    if (!lng) lng = fallbackLng
   
-    // // Redirect if lng in path is not supported
-    // if (
-    //   !languages.some(loc => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
-    //   !req.nextUrl.pathname.startsWith('/_next')
-    // ) {
-    //   return NextResponse.redirect(new URL(`/${lng}${req.nextUrl.pathname}`, req.url))
-    // }
+    // Redirect if lng in path is not supported
+    if (
+      !languages.some(loc => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
+      !req.nextUrl.pathname.startsWith('/_next')
+    ) {
+      return NextResponse.redirect(new URL(`/${lng}${req.nextUrl.pathname}`, req.url))
+    }
   
-    // if (req.headers.has('referer')) {
-    //   const refererUrl = new URL(req.headers.get('referer'))
-    //   const lngInReferer = languages.find((l) => refererUrl.pathname.startsWith(`/${l}`))
-    //   const response = NextResponse.next()
-    //   if (lngInReferer) response.cookies.set(cookieName, lngInReferer)
-    //   return response
-    // }
+    if (req.headers.has('referer')) {
+      const refererUrl = new URL(req.headers.get('referer'))
+      const lngInReferer = languages.find((l) => refererUrl.pathname.startsWith(`/${l}`))
+      const response = NextResponse.next()
+      if (lngInReferer) response.cookies.set(cookieName, lngInReferer)
+      return response
+    }
   
     return NextResponse.next()
   }
